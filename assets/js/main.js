9eb690386b31da8bea4ac5e459de95f0b6cafd17
Dylan Beattie <dylan@dylanbeattie.net>
2019-08-14 01:30:24 +0100

    SHIP IT.

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,3 +65,66 @@
 				});
 
 })(jQuery);
+
+$(function() {
+	const copyToClipboard = (function initClipboardText() {
+		const textarea = document.createElement('textarea');
+	
+		// Move it off screen.
+		textarea.style.cssText = 'position: absolute; left: -99999em';
+	
+		// Set to readonly to prevent mobile devices opening a keyboard when
+		// text is .select()'ed.
+		textarea.setAttribute('readonly', true);
+	
+		document.body.appendChild(textarea);
+	
+		return function setClipboardText(text) {
+			textarea.value = text;
+	
+			// Check if there is any content selected previously.
+			const selected = document.getSelection().rangeCount > 0 ?
+				document.getSelection().getRangeAt(0) : false;
+	
+			// iOS Safari blocks programmtic execCommand copying normally, without this hack.
+			// https://stackoverflow.com/questions/34045777/copy-to-clipboard-using-javascript-in-ios
+			if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+				const editable = textarea.contentEditable;
+				textarea.contentEditable = true;
+				const range = document.createRange();
+				range.selectNodeContents(textarea);
+				const sel = window.getSelection();
+				sel.removeAllRanges();
+				sel.addRange(range);
+				textarea.setSelectionRange(0, 999999);
+				textarea.contentEditable = editable;
+			} else {
+				textarea.select();
+			}
+	
+			try {
+				const result = document.execCommand('copy');
+	
+				// Restore previous selection.
+				if (selected) {
+					document.getSelection().removeAllRanges();
+					document.getSelection().addRange(selected);
+				}
+	
+				return result;
+			} catch (err) {
+				return false;
+			}
+		};
+	})();
+
+	$(".clipboard-links a").on("click", function() {
+		$this = $(this);
+		srcElementId = $this.data('src-element-id');
+		srcElement = $("#" + srcElementId);
+		content = srcElement.val();
+		copyToClipboard(content);
+		alert('Copied to clipboard!');
+		return(false);
+	});
+});
